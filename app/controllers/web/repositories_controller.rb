45f62d075fbbfc96c1ebc110b7de537ac33b09c1
Nikita Konovalov <mipo4t@gmail.com>
2024-04-27 07:45:40 +0400

    prettify repos fetching

--- app/controllers/web/repositories_controller.rb
+++ app/controllers/web/repositories_controller.rb
@@ -2,6 +2,7 @@
 
 class Web::RepositoriesController < Web::ApplicationController
   before_action :authenticate_user!
+  before_action :set_github_repos, only: %i[new create]
 
   def index
     @repositories = current_user.repositories.order(updated_at: :desc)
@@ -9,13 +10,23 @@ class Web::RepositoriesController < Web::ApplicationController
 
   def new
     @repository = Repository.new
-
-    @github_repos = Rails.cache.fetch([current_user.id, session[:login_ts], :repos], expires_in: 10.minutes) do
-      GithubHelper.fetch_user_repos(current_user)
-    end
   end
 
   def create
+    @repository = current_user.repositories.build(repository_params)
+
     redirect_to repositories_path
+  end
+
+  private
+
+  def repository_params
+    params.require(:repository).permit(:github_id)
+  end
+
+  def set_github_repos
+    @github_repos = Rails.cache.fetch([current_user.id, session[:login_ts], :repos], expires_in: 10.minutes) do
+      GithubHelper.fetch_available_user_repos(current_user)
+    end
   end
 end
--- lib/github_helper.rb
+++ lib/github_helper.rb
@@ -2,11 +2,17 @@
 
 module GithubHelper
   class << self
-    def fetch_user_repos(user)
-      client = ApplicationContainer[:octokit_client].new access_token: user.token, auto_paginate: true
+    def client(user)
+      ApplicationContainer[:octokit_client].new access_token: user.token, auto_paginate: true
+    end
 
-      client.repos
-            .select { |r| Repository.language.values.include? r.language.downcase }
+    def fetch_available_user_repos(user)
+      client(user).repos
+                  .select { |r| Repository.language.values.include? r.language.downcase }
+    end
+
+    def fetch_user_repo(user, github_id)
+      client(user).repo(github_id)
     end
   end
 end
