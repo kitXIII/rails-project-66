bd652cee48c10cdde6cdaa2abeefea2e67a25a2b
Nikita Konovalov <mipo4t@gmail.com>
2024-04-29 20:40:08 +0400

    add Repositories::Check model

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,11 +4,12 @@ class Repository < ApplicationRecord
   include AASM
   extend Enumerize
 
+  has_many :checks, dependent: :destroy
   belongs_to :user, inverse_of: 'repositories'
 
   validates :github_id, presence: true, uniqueness: true
 
-  enumerize :language, in: [:ruby]
+  enumerize :language, in: %i[ruby]
 
   aasm column: :fetching_state do
     state :init, initial: true
---
+++ app/models/repository/check.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Repository::Check < ApplicationRecord
+  belongs_to :repository
+
+  aasm column: :state do
+    state :init, initial: true
+    state :fetching, :fetched, :checking, :checked, :failed
+
+    event :fetch do
+      transitions from: %i[init failed], to: :fetching
+    end
+
+    event :mark_as_fetched do
+      transitions from: :fetching, to: :fetched
+    end
+
+    event :check do
+      transitions from: :fetched, to: :checking
+    end
+
+    event :mark_as_checked do
+      transitions from: :checking, to: :checked
+    end
+
+    event :mark_as_failed do
+      transitions to: :failed
+    end
+  end
+end
