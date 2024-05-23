6f0d968ff6426b76db5f335c5a63ef3e187e2d74
Nikita Konovalov <mipo4t@gmail.com>
2024-05-23 11:05:31 +0400

    fix linter errors

--- lib/linter.rb
+++ lib/linter.rb
@@ -3,5 +3,4 @@
 module Linter
   File = Struct.new(:path, :flaws)
   Flaw = Struct.new(:rule, :message, :location)
-  Result = Struct.new(:files)
 end
--- lib/linter/ruby.rb
+++ lib/linter/ruby.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-module Linter::Ruby
-  class << self
-    def command
+module Linter
+  module Ruby
+    def self.command
       "rubocop -f json -c #{Rails.root.join('lib/linter/configs/rubocop.yml')}"
     end
 
-    def transform(result)
+    def self.transform(result)
       result['files']
         .reject { |file| file['offenses'].empty? }
         .map do |file|
-        flaws = file['offenses'].map do |offense|
-          Linter::Flaw.new(
-            rule: offense['cop_name'],
-            message: offense['message'],
-            location: "#{offense['location']['line']}:#{offense['location']['column']}"
-          )
-        end
+          flaws = file['offenses'].map do |offense|
+            Linter::Flaw.new(
+              rule: offense['cop_name'],
+              message: offense['message'],
+              location: "#{offense['location']['line']}:#{offense['location']['column']}"
+            )
+          end
 
-        Linter::File.new(path: file['path'], flaws:)
-      end
+          Linter::File.new(path: file['path'], flaws:)
+        end
     end
   end
 end
