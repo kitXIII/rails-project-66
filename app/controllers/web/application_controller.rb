be4115de348c252417a3c345952138282abab2b0
Nikita Konovalov <mipo4t@gmail.com>
2024-06-02 09:04:28 +0400

    move settings of web controller to web controller

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class Web::ApplicationController < ApplicationController
+  include AuthConcern
+  include Pundit::Authorization
+
+  helper_method :current_user, :signed_in?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+
+    alert = t "#{policy_name}.#{exception.query}", scope: :pundit, default: :default
+    redirect_back(fallback_location: root_path, alert:)
+  end
 end
