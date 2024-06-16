d3e5c04c6d9297b03fa4046c80821626dc8a5478
Nikita Konovalov <mipo4t@gmail.com>
2024-06-16 11:49:26 +0400

    refactor

--- app/mailers/repository_check_mailer.rb
+++ app/mailers/repository_check_mailer.rb
@@ -2,16 +2,21 @@
 
 class RepositoryCheckMailer < ApplicationMailer
   def check_failed_email(check_id)
-    @check = Repository::Check.find(check_id)
-    @repo_name = @check.repository.full_name
+    setup(check_id)
 
     mail(to: @check.repository.user.email, subject: t('.check_failed', repo_name: @repo_name))
   end
 
   def check_found_flaws_email(check_id)
-    @check = Repository::Check.find(check_id)
-    @repo_name = @check.repository.full_name
+    setup(check_id)
 
     mail(to: @check.repository.user.email, subject: t('.check_found_flaws', repo_name: @repo_name))
+  end
+
+  private
+
+  def setup(check_id)
+    @check = Repository::Check.find(check_id)
+    @repo_name = @check.repository.full_name
   end
 end
